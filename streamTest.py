5da81b07f016083cae1d67d31454e6e8fe06383f
Carson Holgate <clholgat@ncsu.edu>
2011-10-12 13:40:28 -0400

    updating

--- streamTest.py
+++ streamTest.py
@@ -1,10 +1,22 @@
-import pycurl, json
+import pycurl, json, urllib
 
-def on_recieve(data):
-	print(data)
-
-conn = pycurl.Curl()
-conn.setopt(pycurl.USERPWD, "clholgat:Jumbothedog12")
-conn.setopt(pycurl.URL, "https://stream.twitter.com/1/statuses/sample.json")
-conn.setopt(pycurl.WRITEFUNCTION, on_recieve)
-conn.perform()
+class bufferedStream:
+	def __init__(self):
+		self.buffer = ""
+		self.conn = pycurl.Curl()
+		self.conn.setopt(pycurl.USERPWD, "clholgat:Jumbothedog12")
+		self.conn.setopt(pycurl.URL, "https://stream.twitter.com/1/statuses/filter.json")
+		self.conn.setopt(pycurl.POST, 1)
+		postOpts = {'track': 'lang:en'}
+		self.conn.setopt(pycurl.POSTFIELDS, urllib.urlencode(postOpts))
+		self.conn.setopt(pycurl.WRITEFUNCTION, self.on_recieve)
+		self.conn.perform()
+		
+	def on_recieve(self, data):
+		self.buffer += data
+		if data.endswith("\r\n") and self.buffer.strip():
+			print(json.loads(self.buffer))
+			self.buffer = ""
+			
+if __name__ == "__main__":
+	stream = bufferedStream()
